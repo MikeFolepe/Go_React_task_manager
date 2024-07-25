3a6cd3a433dda7e9eb7fae43a95ccc349a212d72
MikeFolepe <davyfolepe70@gmail.com>
2024-07-25 04:08:04 -0400

    go app setup

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	fmt.Println("Hello, World!")  
+	app := fiber.New()
+	app.Listen(":4000")
+	log.Fatal(app.Listen(":4000"))
+}
